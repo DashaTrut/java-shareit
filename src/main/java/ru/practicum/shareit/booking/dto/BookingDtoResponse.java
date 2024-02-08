257e56982e86b4097eb7da6156eb5ca831ae947e
DashaBabenko <d.trutneva@yandex.ru>
2024-02-09 00:00:52 +0300

    14sprint 3iteration, 2check

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDtoResponse.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDtoResponse.java
@@ -3,14 +3,12 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
 @Data
 @Builder
@@ -29,16 +27,4 @@ public class BookingDtoResponse {
     @NotNull
     private Status status;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Booking booking = (Booking) o;
-        return Objects.equals(getId(), booking.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        return getClass().hashCode();
-    }
 }
